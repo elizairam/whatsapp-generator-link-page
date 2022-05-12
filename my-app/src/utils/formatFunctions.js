3ea18df2eb93539e309b974d170b7fd99a52cd30
elizabete silva <57414154+elizairam@users.noreply.github.com>
2022-05-12 19:37:43 -0300

    fix: delete old directory

--- my-app/src/utils/formatFunctions.js
+++
@@ -1,9 +0,0 @@
-const formatMobileNumber = (num) => {
-  return parseInt(55 + num);
-};
-
-const formatMessage = (msg) => {
-  return msg.replace(/\s/g, "%20");
-};
-
-export { formatMobileNumber, formatMessage };
