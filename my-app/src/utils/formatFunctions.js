6b792bf08a7204f8289f2f561224e00901eaa7fc
elizabete silva <elizabete.airam@gmail.com>
2022-05-11 21:55:15 -0300

    feat: add form and generator whatsapp link 🟢

---
+++ my-app/src/utils/formatFunctions.js
@@ -0,0 +1,13 @@
+
+const formatMobileNumber = (num) => {
+  return parseInt(55 + num);
+};
+
+const formatMessage = (msg) => {
+  return msg.replace(/\s/g, "%20");
+};
+
+export {
+  formatMobileNumber,
+  formatMessage
+}
